84ac714199437363bd9a01034df98ce84e263970
Erick Felipe <erickfelipe2010@gmail.com>
2022-02-14 08:15:27 -0300

    destructuring assignment

--- destructuring.js
+++ destructuring.js
@@ -1,4 +1,6 @@
-/* // Retornar a oma do dobro de todos os pares
+//ATRIBUIÇÃO VIA DESESTRUTURAÇÃO (DESTRUCTURING ASSIGNMENT)
+
+// Retornar a soma do dobro de todos os pares
 // FILTER+MAP+REDUCE
 // Filtrar pares
 // Dobrar os valores
@@ -15,7 +17,6 @@ const numerosImpares = numeros.filter((valor) => {
 //Pares [ 50, 80, 2, 8, 20, 30 ]
 console.log("Pares", numerosPares);
 console.log("Impares", numerosImpares);
- */
 
 
 //pegar o resto de algo se usa o ...rest operator 
@@ -24,3 +25,24 @@ const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90];
 const [firstNumber, secondNumber, ...rest] = numeros;
 console.log(firstNumber, secondNumber);
 console.log(rest)
+
+
+//Consultando indices
+//                  0       1 
+//                0 1 2   0 1 2 
+const numeros = [[1,2,3],[4,5,6]]
+console.log(numeros[1][2]) 
+
+
+const pessoa = {
+  nome: "José",
+  sobrenome: "Silva",
+  idade: 20,
+  endereco: {
+    rua: "Av Brasil",
+    numero: 999,
+  },
+};
+//Atribuição via desestruturação com chave;
+const { nome, sobrenome, ...r } = pessoa;
+console.log(nome, sobrenome, r);
