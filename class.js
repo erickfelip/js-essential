4b07d263089dcb854d5e07f7060765bcfb32c6ec
Erick Felipe <erickfelipe2010@gmail.com>
2022-03-10 08:25:06 -0300

    Class

---
+++ class.js
@@ -0,0 +1,22 @@
+class Person {
+  constructor(name, lastname) {
+    this.name = name;
+    this.lastname = lastname;
+  }
+  //Criando metodos
+  talk() {
+    console.log(`${this.name}está falando.`);
+  }
+}
+//Função construtora
+function Person2(name, lastname) {
+  this.name = name;
+  this.lastname = lastname;
+}
+Person2.prototype.talk = function () {
+  console.log(`${this.name}está falando.`);
+};
+
+//Instanciando uma pessoa //Criando um objeto a partir da classe
+const person1 = new Person("João", "Silva");
+console.log(person1);
